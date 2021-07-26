7c10133107fd51a1a96374e6c5d98aab63ae0ae4
Leidy Ingrid <leidyingrid20@gmail.com>
2021-07-25 22:29:23 -0300

    Se agrega código html y css

--- src/main/java/ar/com/ada/api/questionados/controllers/QuestionadosController.java
+++ src/main/java/ar/com/ada/api/questionados/controllers/QuestionadosController.java
@@ -1,11 +1,16 @@
 package ar.com.ada.api.questionados.controllers;
 
+import java.util.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import ar.com.ada.api.questionados.entities.Pregunta;
+import ar.com.ada.api.questionados.entities.Respuesta;
 import ar.com.ada.api.questionados.models.request.RespuestaAVerificar;
+import ar.com.ada.api.questionados.models.response.OpcionPregunta;
+import ar.com.ada.api.questionados.models.response.PreguntaAResolver;
 import ar.com.ada.api.questionados.models.response.RespuestaVerificada;
 import ar.com.ada.api.questionados.services.*;
 
@@ -18,11 +23,13 @@ public class QuestionadosController {
 
     //GET /questionados/next
     @GetMapping("/questionados/next")
-    public ResponseEntity<Pregunta> traerPreguntaRandom() {
+    public ResponseEntity<PreguntaAResolver> traerPreguntaRandom() {
 
-        Pregunta proximaPregunta = service.traerPreguntaRandom();
+        Pregunta pregunta = service.traerPreguntaRandom();
 
-        return ResponseEntity.ok(proximaPregunta);
+        PreguntaAResolver preguntaAResolver = PreguntaAResolver.convertirDesde(pregunta);
+
+        return ResponseEntity.ok(preguntaAResolver);
 
         //return ResponseEntity.ok(service.traerPreguntaRandom());
 
@@ -30,8 +37,7 @@ public class QuestionadosController {
 
     //verificar si una respuesta de una pregunta es valida
     @PostMapping("/questionados/verificaciones")
-    public ResponseEntity<RespuestaVerificada> verificarRespuesta(
-            @RequestBody RespuestaAVerificar respuestaAVerificar) {
+    public ResponseEntity<RespuestaVerificada> verificarRespuesta(@RequestBody RespuestaAVerificar respuestaAVerificar) {
 
         RespuestaVerificada respuestaVerificada = new RespuestaVerificada();
         if (service.verificarRespuesta(respuestaAVerificar.preguntaId, respuestaAVerificar.respuestaId)) {
